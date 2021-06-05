e8364686dd15a30eb15f2fd5a94131e9c327fad2
YuriFA <yurik45gb@gmail.com>
2021-06-05 14:36:39 +0300

    fix index page

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,22 @@
-console.log('Hello world!')
+import { DOMReady } from './utils/DOMReady';
+
+const initStickyHeader = () => {
+  const header = document.querySelector('.header');
+  const sticky = header.offsetTop + 50;
+
+  function handleScroll() {
+    if (window.pageYOffset > sticky) {
+      header.classList.add('sticky');
+    } else {
+      header.classList.remove('sticky');
+    }
+  }
+
+  // When the user scrolls the page, execute myFunction
+  window.addEventListener('scroll', handleScroll);
+  handleScroll();
+};
+
+DOMReady(() => {
+  initStickyHeader();
+});
---
+++ src/js/utils/DOMReady.js
@@ -0,0 +1,5 @@
+export const DOMReady = callback => {
+  document.readyState === 'interactive' || document.readyState === 'complete'
+    ? callback()
+    : document.addEventListener('DOMContentLoaded', callback);
+};
