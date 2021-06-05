dd242d891624132757b190bac41dce9fde30bc3c
YuriFA <yurik45gb@gmail.com>
2021-06-05 17:28:18 +0300

    add mobile styles

--- src/js/index.js
+++ src/js/index.js
@@ -17,6 +17,52 @@ const initStickyHeader = () => {
   handleScroll();
 };
 
+const initSidebar = () => {
+  const CLASSES = {
+    TRIGGER: 'sidenav-trigger',
+    TRIGGER_OPEN: 'sidenav-trigger--open',
+  };
+
+  const SIDEBAR_ID = 'sidenav';
+
+  const sidebarTrigger = Array.from(
+    document.querySelectorAll(`.${CLASSES.TRIGGER}`),
+  );
+
+  let isOpen = false;
+
+  function openNav() {
+    isOpen = true;
+    document.body.style.overflow = 'hidden';
+    document.getElementById(SIDEBAR_ID).style.transform = `translateX(0)`;
+  }
+
+  function closeNav() {
+    isOpen = false;
+    document.body.style.overflow = null;
+    document.getElementById(SIDEBAR_ID).style.transform = `translateX(100%)`;
+  }
+
+  if (sidebarTrigger) {
+    sidebarTrigger.forEach((element) => {
+      element.addEventListener('click', () => {
+        if (isOpen) {
+          sidebarTrigger.forEach((element) => {
+            element.classList.remove(CLASSES.TRIGGER_OPEN);
+          });
+          closeNav();
+        } else {
+          sidebarTrigger.forEach((element) => {
+            element.classList.add(CLASSES.TRIGGER_OPEN);
+          });
+          openNav();
+        }
+      });
+    });
+  }
+};
+
 DOMReady(() => {
   initStickyHeader();
+  initSidebar();
 });
